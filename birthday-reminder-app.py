9a7735723e668ec2ac944f0243b623c48b40bbca
zsusinskis <zsusinskis@gmail.com>
2023-10-15 18:19:03 +0300

    Created database, implemented DB

--- birthday-reminder-app.py
+++ birthday-reminder-app.py
@@ -12,6 +12,7 @@ conf_path = dir_path + "\\" + conf_name
 db_name = None
 db_path = dir_path + "\\"
 
+#If config exists, reads the config's values
 if os.path.exists(conf_path):
     config = configparser.ConfigParser()
     config.read(conf_path)
@@ -20,3 +21,29 @@ if os.path.exists(conf_path):
 else:
     print("Config file has not been found!")
 
+connection = sqlite3.connect(db_path)
+cursor = connection.cursor()
+
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='birthdays'")
+reults = cursor.fetchone()
+
+#Checks if database is empty, if it is - creates table birthdays
+if not reults:
+    cursor.execute("""CREATE TABLE IF NOT EXISTS birthdays (
+               id SERIAL PRIMARY KEY NOT NULL,
+               name TEXT NOT NULL,
+               lastname TEXT NOT NULL,
+               birthday DATE NOT NULL );
+    """)
+    print("Created table birthdays")
+
+print("""
+    /Main MENU/
+    1 - add a new birthday
+    2 - remove birthday
+    3 - check all birthdays
+""")
+
+cursor.close()
+connection.commit()
+connection.close()
